534fc84b49d81ef1f0ad44928b4eae5a662f0849
osshiya <osshiya@gmail.com>
2024-02-19 05:14:38 +0800

    refactor auth

--- flutter_app/lib/models/authDB.dart
+++ flutter_app/lib/models/authDB.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -32,43 +33,6 @@ class FirebaseHelper {
     return '';
   }
 
-  // Future<Map<String, dynamic>?> getUserList(String uid) async {
-  //   DocumentSnapshot<Map<String, dynamic>> snapshot =
-  //       await _firestore.collection('users').doc(uid).get();
-
-  //   if (snapshot.exists) {
-  //     Map<String, dynamic>? data = snapshot.data();
-
-  //     // if (data != null && data.containsKey('username')) {
-  //     //   return data['username'].toString();
-  //     // }
-  //     return data;
-  //   }
-  //   return null;
-
-  //   // Return an empty string if the username is not found or if the snapshot doesn't exist
-  //   // return '';
-  // }
-  // Assume 'userId' is the ID of the user for whom you want to retrieve sub-users
-  Future<List<Map<String, dynamic>>> getSubUsers(String uid) async {
-    try {
-      CollectionReference<Map<String, dynamic>> subUsersCollection =
-          _firestore.collection('users/$uid/subUsers');
-
-      QuerySnapshot<Map<String, dynamic>> subUsersSnapshot =
-          await subUsersCollection.get();
-
-      List<Map<String, dynamic>> subUsersData = subUsersSnapshot.docs
-          .map((QueryDocumentSnapshot<Map<String, dynamic>> doc) => doc.data())
-          .toList();
-
-      return subUsersData;
-    } catch (e) {
-      print("Error retrieving sub-users: $e");
-      return [];
-    }
-  }
-
   Future<Map<String, dynamic>?> getUserData(String uid) async {
     if (uid != null && uid.isNotEmpty) {
       DocumentSnapshot<Map<String, dynamic>> snapshot =
@@ -95,5 +59,20 @@ class FirebaseHelper {
     }
     // Return an empty string if the username is not found or if the snapshot doesn't exist
     // return '';
+  }
+
+  // Assuming UserCredential is obtained after registration
+  Future<void> storeUserData(UserCredential user) async {
+    try {
+      final newUid = user.user!.uid;
+      final newEmail = user.user!.email;
+
+      await _firestore
+          .collection('users')
+          .doc(newUid)
+          .set({'uid': newUid, 'email': newEmail});
+    } catch (e) {
+      print('Error storing user data: $e');
+    }
   }
 }
