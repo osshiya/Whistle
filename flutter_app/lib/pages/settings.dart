749258a30dd224aa3e8aeac97b48890ef3d88073
osshiya <osshiya@gmail.com>
2024-02-21 10:32:02 +0800

    add settings

--- flutter_app/lib/pages/settings.dart
+++ flutter_app/lib/pages/settings.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_app/auth/login_page.dart';
+import 'package:flutter_app/models/authDB.dart' as AuthDB;
 
 class SettingsPage extends StatefulWidget {
   static const title = 'Settings';
@@ -16,7 +17,33 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  late AuthDB.FirebaseHelper dbAuthHelper;
   late String uid;
+  Map<String, dynamic>? _data;
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emergencyNumberController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    dbAuthHelper = AuthDB.FirebaseHelper();
+
+    _retrieveData();
+  }
+
+  Future<void> _retrieveData() async {
+    try {
+      uid = await dbAuthHelper.getStoredUid();
+      Map<String, dynamic>? data = await dbAuthHelper.getUserData(uid);
+      setState(() {
+        _data = data;
+        _nameController.text = _data?['name'] ?? '';
+        _emergencyNumberController.text = _data?['emergencyNumber'] ?? '';
+      });
+    } catch (error) {
+      print('Error retrieving data: $error');
+    }
+  }
 
   Future<void> _logout() async {
     try {
@@ -55,15 +82,58 @@ class _SettingsPageState extends State<SettingsPage> {
       appBar: AppBar(
         title: const Text(SettingsPage.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-              'Welcome to the Settings!',
-              style: TextStyle(fontSize: 20.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Change Name:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your new name',
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 16.0),
+            SizedBox(height: 20.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Change Emergency Number:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextField(
+                  controller: _emergencyNumberController,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your new emergency number',
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Save the changes
+                String newName = _nameController.text;
+                String newEmergencyNumber = _emergencyNumberController.text;
+                dbAuthHelper.updateUserData(newName, newEmergencyNumber);
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Changes saved successfully')),
+                );
+              },
+              child: Text('Save Changes'),
+            ),
             const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () => _logout(),
